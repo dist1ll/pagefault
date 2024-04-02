2844f267591ecc9261416c7cb8db3f471cc42279
Adrian Alic <contact@alic.dev>
2024-04-02 09:43:55 +0000

    .

--- multithreaded.c
+++ multithreaded.c
@@ -1,4 +1,7 @@
+#define _GNU_SOURCE
+
 #include "time.h"
+#include <sched.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <sys/mman.h>
@@ -8,14 +11,22 @@
 struct args {
   char *addr;
   size_t len;
+  int cpu;
 };
 
 int prefault(void *arg) {
   struct args *a = (struct args *)arg;
-  madvise(a->addr, a->len, MADV_POPULATE_WRITE);
-  // for (size_t i = 0; i < a->len; i += 4096) {
-  //   a->addr[i] = 0xff;
-  // }
+  /* set affinity */
+  cpu_set_t my_set;         /* Define your cpu_set bit mask. */
+  CPU_ZERO(&my_set);        /* Initialize it all to 0, i.e. no CPUs selected. */
+  CPU_SET(a->cpu, &my_set); /* set the bit that represents core 7. */
+  sched_setaffinity(0, sizeof(cpu_set_t), &my_set);
+
+  // madvise(a->addr, a->len, MADV_POPULATE_WRITE);
+  printf("I'm currently on CPU: %i\n", sched_getcpu());
+  for (size_t i = 0; i < a->len; i += 4096) {
+    a->addr[i] = 0xff;
+  }
   return 0;
 }
 
@@ -31,6 +42,7 @@ void iter(int threads) {
   for (int i = 0; i < threads; i++) {
     as[i].addr = tmp;
     as[i].len = map_size / threads;
+    as[i].cpu = i;
     thrd_create(&ids[i], prefault, (void *)&as[i]);
     tmp += map_size / threads;
   }
@@ -43,6 +55,4 @@ void iter(int threads) {
   munmap((void *)addr, map_size);
 }
 
-int main(int argc, char *argv[]) {
-  iter(..)
-}
+int main(int argc, char *argv[]) { iter(1); }
